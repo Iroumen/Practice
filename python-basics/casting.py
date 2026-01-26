13b4c565b5adefe359bbfae996d13cd93ade3b1b
Iroumen <aybinanuar@gmail.com>
2026-01-26 23:33:52 +0500

    Finishing practice1

---
+++ python-basics/casting.py
@@ -0,0 +1,15 @@
+a = int(1)   # a will be 1
+b = int(2.8) # b will be 2
+c = int("3") # c will be 3
+
+
+a = float(1)     # a will be 1.0
+b = float(2.8)   # b will be 2.8
+c = float("3")   # c will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+a = str("s1") # a will be 's1'
+b = str(2)    # b will be '2'
+c = str(3.0)  # c will be '3.0'
+
